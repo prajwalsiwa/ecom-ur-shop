bee167fb057fa8f976d4a6af63318595bde6c987
prajwalsiwa <prajwal.siwa1@gmail.com>
2025-08-28 21:18:27 +0545

    feat: add pagination component

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import React from "react";
+
+interface PaginationProps {
+  totalPages: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+}
+
+export default function Pagination({
+  totalPages,
+  currentPage,
+  onPageChange,
+}: PaginationProps) {
+  if (totalPages <= 1) return null;
+
+  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+
+  return (
+    <div className="flex justify-center mt-6 space-x-2">
+      <button
+        className="px-3 py-1 rounded border hover:bg-gray-200 disabled:opacity-50"
+        onClick={() => onPageChange(currentPage - 1)}
+        disabled={currentPage === 1}
+      >
+        Prev
+      </button>
+
+    
+      {pages.map((page) => (
+        <button
+          key={page}
+          onClick={() => onPageChange(page)}
+          className={`px-3 py-1 rounded border hover:bg-gray-200 ${
+            page === currentPage ? "bg-gray-600 text-white border-gray-600" : ""
+          }`}
+        >
+          {page}
+        </button>
+      ))}
+
+      <button
+        className="px-3 py-1 rounded border hover:bg-gray-200 disabled:opacity-50"
+        onClick={() => onPageChange(currentPage + 1)}
+        disabled={currentPage === totalPages}
+      >
+        Next
+      </button>
+    </div>
+  );
+}
