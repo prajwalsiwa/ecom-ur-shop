9f6867675b365e6ee1f9fea6db3f7e1050f130e8
prajwalsiwa <prajwal.siwa1@gmail.com>
2025-08-28 21:20:50 +0545

    feat: implement product listing with pagination and loading states

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,47 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+import { useEffect, useState } from "react";
+import { fetcher } from "@/lib/fetcher";
+import ProductCard from "@/components/ProductCard";
+import ProductGridSkeleton from "@/components/ProductGridSkeleton";
+import Pagination from "@/components/Pagination"; 
 
+const PRODUCTS_PER_PAGE = 6;
 
 export default function Home() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [products, setProducts] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetcher("https://fakestoreapi.com/products")
+      .then((data) => setProducts(data))
+      .catch(() => setError("Unable to fetch products"))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) return <ProductGridSkeleton />;
+  if (error) return <div className="text-red-500 text-center">{error}</div>;
+
+
+  const totalPages = Math.ceil(products.length / PRODUCTS_PER_PAGE);
+  const startIndex = (currentPage - 1) * PRODUCTS_PER_PAGE;
+  const currentProducts = products.slice(startIndex, startIndex + PRODUCTS_PER_PAGE);
+
   return (
-    <>
-      <div className="text-red-500 font-bold">Hello</div>
-      <div className="bg-blue-600 text-white">World</div>
-    </>
+    <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {currentProducts.map((p) => (
+          <ProductCard key={p.id} product={p} />
+        ))}
+      </div>
+
+      <Pagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        onPageChange={(page) => setCurrentPage(page)}
+      />
+    </div>
   );
 }
